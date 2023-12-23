0b063b43daa4d9d7afa1c9233d8d7a86b5d91d07
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-12-23 12:13:07 +0545

    refactor: define checkStorage and checkMessaage

--- popup/index.js
+++ popup/index.js
@@ -1,67 +1,94 @@
 var msg_div = document.getElementById("msg")
+var time_container = document.getElementById("time-container")
+var msg_para = document.getElementById("msg-para")
 var end_btn = document.getElementById("btn-end")
 
-
-browser.storage.local.get("start_time")
+function checkStorage(){	
+browser.storage.local.get("time")
 .then((value) => {
-    msg_div.innerHTML = `In a Loop from ${value} sec`
-})
+  if(value.time){
+    let time = value.time.split('-');
+    let start_time = time[0]
+    let end_time = time[1]
+    msg_para.innerText =  `In a Loop from ${start_time} sec to ${end_time} sec`;
+    time_container.style.display = "none";
+    end_btn.style.display = "block"
+    console.log(value)
+   }
+ })
+}
 
-    let start_hr_i = document.getElementById("start-hr");
-    let start_min_i = document.getElementById("start-min")
-    let start_sec_i = document.getElementById("start-sec")
-    let end_hr_i = document.getElementById("end-hr")
-    let end_min_i = document.getElementById("end-min")
-    let end_sec_i = document.getElementById("end-sec")
-    let start_btn = document.getElementById("btn-start")
+ let start_hr_i = document.getElementById("start-hr");
+ let start_min_i = document.getElementById("start-min")
+ let start_sec_i = document.getElementById("start-sec")
+ let end_hr_i = document.getElementById("end-hr")
+ let end_min_i = document.getElementById("end-min")
+ let end_sec_i = document.getElementById("end-sec")
+ let start_btn = document.getElementById("btn-start")
     
     
-    
-    start_btn.addEventListener("click", () => {
-     try{
-        var start_hr = parseInt(start_hr_i.value);
-        var start_min = parseInt(start_min_i.value);
-        var start_sec = parseInt(start_sec_i.value);
-        var end_hr = parseInt(end_hr_i.value);
-        var end_min = parseInt(end_min_i.value);
-        var end_sec = parseInt(end_sec_i.value);
-    
-        var start_time_in_sec = start_hr * 3600 + start_min * 60 + start_sec;
-        var end_time_in_sec = end_hr * 3600 + end_min * 60 + end_sec;
-        var duration = end_time_in_sec - start_time_in_sec;
+ start_btn.addEventListener("click", () => {
+  try{
 
-        if (duration < 0) {
-            msg_div.innerText = "End Time cannot be greater than start time"
-            return;
-        }
+    var start_hr = parseInt(start_hr_i.value);
+    var start_min = parseInt(start_min_i.value);
+    var start_sec = parseInt(start_sec_i.value);
+    var end_hr = parseInt(end_hr_i.value);
+    var end_min = parseInt(end_min_i.value);
+    var end_sec = parseInt(end_sec_i.value);
     
-        if (start_time_in_sec === end_time_in_sec) {
-            msg_div.innerText = "The Start and End Time are Same";
-            return;
-        }
+    var start_time_in_sec = start_hr * 3600 + start_min * 60 + start_sec;
+    var end_time_in_sec = end_hr * 3600 + end_min * 60 + end_sec;
+    var duration = end_time_in_sec - start_time_in_sec;
+
+    if(start_hr_i.value.length == 0 || start_min_i.value.length == 0 || start_sec_i.value.length == 0 || end_hr_i.value.length == 0 || end_min_i.value.length == 0 || end_sec_i.value.length == 0){
+      msg_para.innerText = "All the fields are required"	    
+    }
+
+     else if(duration < 0) {
+       msg_para.innerText = "End Time cannot be greater than start time"
+     }
     
-        if (start_sec && end_sec || start_hr && end_hr || start_min && end_min) {
-            console.log("clicked")
-        } else {
-            msg_div.innerText = "All Fields are required"
-        }
-    
-        browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            var activeTab = tabs[0];
-            browser.tabs.sendMessage(activeTab.id, { start_time: start_time_in_sec, end_time: end_time_in_sec });
-        });
-      } catch(e){
-         msg_div.innerText = "Invalid Input"
-         return;
+     else if (start_time_in_sec === end_time_in_sec) {
+       msg_para.innerText = "The Start and End Time are Same";
       }
+     else{
+     browser.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+         var activeTab = tabs[0];
+	 browser.tabs.sendMessage(activeTab.id, { start_time: start_time_in_sec, end_time: end_time_in_sec, id: activeTab.id });
+         time_container.style.display = "none";
+         msg_div.style.display = "block";
+	 checkMessge();
+      });
+     }
+     } catch(e){
+        msg_para.innerText = "Invalid Input"
+     }
     })
 
+function checkMessge(){
+ browser.runtime.onMessage.addListener((message) => {
+   if(message.status == 0){
+     msg_para.innerText = "Duration too long"
+     time_container.style.display = "block";
+   } else {
+     checkStorage()	   
+   }	 
+ })
+  	
+}
 
-end_btn.addEventListener("click", () => {
+if(end_btn){
+  end_btn.addEventListener("click", () => {
     browser.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         var activeTab = tabs[0];
         browser.tabs.sendMessage(activeTab.id, { start_time: null });
     });
-})
+  time_container.style.display = "block";
+  msg_div.style.display = "none";
 
+ })
+}
+
+checkStorage();
 
