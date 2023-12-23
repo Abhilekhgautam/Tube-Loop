c864a931668deeb92249a8b8b58270bdec49e888
abhilekhgautam <abhilekhgautam119@gmail.com>
2023-12-23 07:55:38 +0545

    refactor: use local storage for messages

--- main.js
+++ main.js
@@ -2,18 +2,20 @@ var id;
 
 browser.runtime.onMessage.addListener((message) => {
     // Handle the message here
-    if(message.hasOwnProperty('start_time')){
+    if(message.start_time){
         let start_time_in_sec = message.start_time;
         let end_time_in_sec = message.end_time;
      
         let media = document.querySelector("video")
         media.currentTime = start_time_in_sec;
+
+        browser.storage.local.set({start_time: start_time_in_sec, end_time: end_time_in_sec})
         
         id = setInterval(() =>{
          media.currentTime = start_time_in_sec
          console.log("Reset");
         }, (end_time_in_sec - start_time_in_sec) * 1000)
-    } else if (message.hasOwnProperty('message')){
+    } else {
         clearInterval(id);
     }
 });
